9b4f92fa8a3454cf3b94ab19520740c2281bf55f
GreyWoulf1 <93624560+GreyWoulf@users.noreply.github.com>
2022-05-10 22:22:05 +0000

    updated main.sql file

--- main.sql
+++ main.sql
@@ -32,3 +32,24 @@
 --   OR neighborhood = 'Downtown'
 --   OR neighborhood = 'Chinatown'
 -- ORDER BY neighborhood ASC;
+
+
+
+-- SELECT *
+-- FROM nomnom
+-- WHERE health IS NULL;
+
+-- SELECT *
+-- FROM nomnom
+-- ORDER BY review DESC
+-- LIMIT 10;
+
+-- SELECT name,
+--   CASE
+--    WHEN review > 4.5 THEN 'Extraordinary'
+--    WHEN review > 4 THEN 'Excellent'
+--    WHEN review > 3 THEN 'Good'
+--    WHEN review > 2 THEN 'Fair'
+--    ELSE 'Poor'
+--   END AS 'Rating'
+-- FROM nomnom;
