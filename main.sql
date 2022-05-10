3019f0e24c02e02b4688b08aa25fb250650c92a9
GreyWoulf1 <93624560+GreyWoulf@users.noreply.github.com>
2022-05-10 22:07:54 +0000

    added more SELECT features to get access to requested data

--- main.sql
+++ main.sql
@@ -15,3 +15,20 @@
 -- FROM nomnom
 -- WHERE review > 3
 
+
+
+-- SELECT *
+-- FROM nomnom
+-- WHERE cuisine = 'Italian'
+--   AND price = '$$$'
+
+-- SELECT *
+-- FROM nomnom
+-- WHERE name LIKE '%meatball%';
+
+-- SELECT *
+-- FROM nomnom
+-- WHERE neighborhood = 'Midtown'
+--   OR neighborhood = 'Downtown'
+--   OR neighborhood = 'Chinatown'
+-- ORDER BY neighborhood ASC;
